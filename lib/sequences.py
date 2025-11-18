df0cc367827d15bfecdeaa54513c93cd46e5b321
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-19 01:20:14 +0300

    Add origin

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib.append(a)
+        a, b = b, a + b
+    print(fib)
